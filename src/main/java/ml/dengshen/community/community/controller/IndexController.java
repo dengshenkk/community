972f798d6e190749335bbf78f46f2566cfe5c5ff
dengshen <dengshenkk@gmail.com>
2020-02-06 16:28:07 +0800

    抽离HTML模板 首页问题分页待实现

--- src/main/java/ml/dengshen/community/community/controller/IndexController.java
+++ src/main/java/ml/dengshen/community/community/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +23,10 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model) {
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(value = "size", defaultValue = "5") Integer size) {
         User user = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
@@ -34,7 +38,7 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList = questionService.list();
+        List<QuestionDTO> questionList = questionService.list(page, size);
         model.addAttribute("questions", questionList);
         return "index";
     }
--- src/main/java/ml/dengshen/community/community/mapper/QuestionMapper.java
+++ src/main/java/ml/dengshen/community/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package ml.dengshen.community.community.mapper;
 import ml.dengshen.community.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,7 +13,7 @@ public interface QuestionMapper {
 
     @Insert("insert into question (title, description,gmt_create, gmt_modify, creator, comment_count, view_count, like_count, tag) values (#{title}, #{description}, #{gmtCreate}, #{gmtModify}, #{creator}, #{commentCount}, #{viewCount}, #{likeCount}, #{tag})")
     void create(Question question);
-
-    @Select("select * from question")
-    List<Question> list();
+//    SELECT * FROM question LIMIT 0,5
+    @Select("select * from question limit #{offset}, #{size}")
+    List<Question> list(@Param(value = "offset") Integer offset, @Param(value = "size") Integer size);
 }
